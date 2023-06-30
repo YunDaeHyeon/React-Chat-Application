a7f58f0f0695ca5384a94ed3aa7a46c89c413ce8
Daehyeon Yun <62231651+YunDaeHyeon@users.noreply.github.com>
2023-06-30 13:47:39 +0000

    feat: CRA 기초작업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+            <h4>TEAMIO</h4>
+            <button>Sign up now</button>
+        </div>
       </header>
+      <div>
+        <figure>
+            <div>이미지 들어갈 자리</div>
+        </figure>
+        <section>
+            <div>
+                <h2>Bring you work<br/>& team together</h2>
+                <p>Work fast and flexible by communicating effectively.</p>
+                <button>Sign up now</button>
+            </div>
+        </section>
+      </div>
+      <section>
+        <div>
+            <p>테스트 문구</p>
+            <image src="./a.png" alt="테스트"/><p>테스트 이름</p>
+        </div>
+      </section>
+      <footer>
+        <div>
+        </div>
+        <div>
+            <h4>테스트</h4>
+            <ul>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+        </div>
+      </footer>
     </div>
   );
 }
