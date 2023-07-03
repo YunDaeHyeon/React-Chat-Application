4746110c8dd17dccd5694f689b9c105d47d4a625
Daehyeon Yun <62231651+YunDaeHyeon@users.noreply.github.com>
2023-07-03 14:23:41 +0000

    feat: index 페이지 디자인 작업

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,5 +1,8 @@
 import "./IndexPage_style.css";
 
+// Chakra import
+import { Image } from '@chakra-ui/react'
+
 function IndexPage() {
     return(
         <div className="index-container">
@@ -8,10 +11,30 @@ function IndexPage() {
                 <button>Sign up now</button>
             </div>
             <div className="index-figure-container">
-                figure 파트
+                <div className="index-figure-left">
+                    <div>
+                        <h2>Bring your work<br/>& team together</h2>
+                        <p>Work fast and flexible by communicating effectively.</p>
+                        <button>Sign up now</button>
+                    </div>
+                </div>
+                <div className="index-figure-right">
+                    <Image
+                        className="index-figure-right-image"
+                        src='https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_1280.jpg' alt='index intro' />
+                </div>
             </div>
             <div className="index-section-container">
-                section 파트
+                <p>"We love using Teamio. Our efficiency grew by 45% and that's amazing!"</p>
+                <div className="index-section-profile">
+                    <Image
+                        className="index-sesction-image"
+                        borderRadius="full"
+                        objectFit="cover"
+                        boxSize="30px"
+                        src="https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_640.jpg"/>
+                    <p className="index-sesction-name">DH Company CEO.</p>
+                </div>
             </div>
             <div className="index-footer-container">
                 footer 파트
