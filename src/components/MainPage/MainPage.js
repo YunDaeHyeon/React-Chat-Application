e852d955b021fa2589ecb3883f69f25415bff0aa
Daehyeon Yun <62231651+YunDaeHyeon@users.noreply.github.com>
2023-07-12 14:43:50 +0000

    feat: 메인페이지 제작 중/23.07.12

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,14 +1,28 @@
 import "./MainPage_style.css";
 
+//react-icons
+import { FaPeopleGroup, FaMessage, FaBell } from "react-icons/fa6";
+import { IoCall } from "react-icons/io5";
+
 function MainPage(){
     return(
         <div className="main-container">
             <div className="navigation-container">
                 <div className="navigation-tab-container">
-                    1
+                    <h2 className="navigation-tab-title-top">WORK</h2>
+                    <h2 className="navigation-tab-title-bottom">BEE</h2>
+                    <FaPeopleGroup className="navigation-tab-icon" size="22px"/>
+                    <FaMessage className="navigation-tab-icon" size="22px"/>
+                    <FaBell className="navigation-tab-icon" size="22px"/>
+                    <IoCall className="navigation-tab-icon" size="22px"/>
                 </div>
                 <div className="navigation-tab-inner">
-                    2
+                    <div className="navigation-tab-content-selector">
+                        선택창
+                    </div>
+                    <div className="navigation-tab-content">
+                        내용
+                    </div>
                 </div>
             </div>
             <div className="content-container">
