0014cfb1f32c351daa179820c755d8195c8b4694
Daehyeon Yun <62231651+YunDaeHyeon@users.noreply.github.com>
2023-07-16 07:57:54 +0000

    feat: babel 설정 추가, emoji 구축 중

--- src/commons/components/ChatInputForm.js
+++ src/commons/components/ChatInputForm.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./ChatInputForm_style.css";
 
 import { TbAlphabetLatin } from "react-icons/tb";
@@ -7,12 +9,33 @@ import { MdRecordVoiceOver } from 'react-icons/md';
 import { BsCameraVideoFill } from 'react-icons/bs';
 import { FaFaceSmile } from 'react-icons/fa6';
 
+import {
+    Popover, PopoverTrigger, PopoverContent, PopoverHeader, PopoverCloseButton,
+    PopoverBody
+} from '@chakra-ui/react';
+import EmojiPicker from "emoji-picker-react";
+
 function ChatInputForm(){
+    const [chatInputValue, setChatInputValue] = useState("");
+    const [clickEmoji, setClickEmoji] = useState(false);
+
+    const onChatInputFormChange = (e) => {
+        setChatInputValue(e.target.value);
+    }
+
+    // 이모지 선택 버튼
+    const onClickEmojiButton = () => {
+        setClickEmoji(prev => !prev);
+        console.log(clickEmoji);
+    }
+    
     return(
         <div className="chat-input-form-container">
             <textarea 
                 className="chat-input-form-textarea"
-                placeholder="Type a new message..."></textarea>
+                placeholder="Type a new message..."
+                onChange={onChatInputFormChange}
+                value={chatInputValue}></textarea>
             <div className="chat-input-form-buttons">
                 <div className="chat-input-form-icons">
                     <TbAlphabetLatin size="24px" style={{marginLeft:"10px"}}/>
@@ -20,7 +43,29 @@ function ChatInputForm(){
                     <BiLink size="24px"/>
                     <MdRecordVoiceOver size="24px" style={{marginLeft:"10px", marginRight:"10px"}}/>
                     <BsCameraVideoFill size="24px"/>
-                    <FaFaceSmile size="24px" style={{marginLeft:"10px", marginRight:"10px"}}/>
+                    <Popover placement='top-start'>
+                        <PopoverTrigger>
+                            <FaFaceSmile 
+                                size="24px" 
+                                style={{
+                                    marginLeft:"10px",
+                                    marginRight:"10px",
+                                    cursor:'pointer',
+                                }}
+                            />
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <PopoverHeader fontWeight='semibold'>이모티콘 선택</PopoverHeader>
+                            <PopoverArrow />
+                            <PopoverCloseButton />
+                            <PopoverBody>
+                                <EmojiPicker
+                                    height={350}
+                                    width="40%"
+                                    autoFocusSearch={false}/>
+                            </PopoverBody>
+                      </PopoverContent>
+                    </Popover>
                 </div>
                 <div className="chat-input-form-send">
                     <BiSolidPaperPlane style={{marginRight:"10px"}}size="24px"/>
