6708f0fe6ee2feab1bb5727312be274cfa275e9d
Daehyeon Yun <62231651+YunDaeHyeon@users.noreply.github.com>
2023-07-16 14:19:24 +0000

    feat: emoji picker 삭제

--- src/commons/components/ChatInputForm.js
+++ src/commons/components/ChatInputForm.js
@@ -9,24 +9,11 @@ import { MdRecordVoiceOver } from 'react-icons/md';
 import { BsCameraVideoFill } from 'react-icons/bs';
 import { FaFaceSmile } from 'react-icons/fa6';
 
-import {
-    Popover, PopoverTrigger, PopoverContent, PopoverHeader, PopoverCloseButton,
-    PopoverBody
-} from '@chakra-ui/react';
-import EmojiPicker from "emoji-picker-react";
-
 function ChatInputForm(){
     const [chatInputValue, setChatInputValue] = useState("");
-    const [clickEmoji, setClickEmoji] = useState(false);
 
     const onChatInputFormChange = (e) => {
         setChatInputValue(e.target.value);
-    }
-
-    // 이모지 선택 버튼
-    const onClickEmojiButton = () => {
-        setClickEmoji(prev => !prev);
-        console.log(clickEmoji);
     }
     
     return(
@@ -43,29 +30,14 @@ function ChatInputForm(){
                     <BiLink size="24px"/>
                     <MdRecordVoiceOver size="24px" style={{marginLeft:"10px", marginRight:"10px"}}/>
                     <BsCameraVideoFill size="24px"/>
-                    <Popover placement='top-start'>
-                        <PopoverTrigger>
-                            <FaFaceSmile 
-                                size="24px" 
-                                style={{
-                                    marginLeft:"10px",
-                                    marginRight:"10px",
-                                    cursor:'pointer',
-                                }}
-                            />
-                        </PopoverTrigger>
-                        <PopoverContent>
-                            <PopoverHeader fontWeight='semibold'>이모티콘 선택</PopoverHeader>
-                            <PopoverArrow />
-                            <PopoverCloseButton />
-                            <PopoverBody>
-                                <EmojiPicker
-                                    height={350}
-                                    width="40%"
-                                    autoFocusSearch={false}/>
-                            </PopoverBody>
-                      </PopoverContent>
-                    </Popover>
+                    <FaFaceSmile 
+                        size="24px" 
+                        style={{
+                            marginLeft:"10px",
+                            marginRight:"10px",
+                            cursor:'pointer',
+                        }}
+                    />
                 </div>
                 <div className="chat-input-form-send">
                     <BiSolidPaperPlane style={{marginRight:"10px"}}size="24px"/>
